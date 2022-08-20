42e7c4886d19f8dae429e5d1a50e138581c90193
Amber bharti <amberb034@gmail.com>
2022-08-20 13:35:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
@@ -9,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/react-redux-crud" element={<Home/>}/>
         <Route exact path="/addUser" element={<AddUser/>}/>
         <Route exact path="/editUser/:id" element={<EditUser/>}/>
       </Routes>
